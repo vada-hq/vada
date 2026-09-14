652453baf020f383b9ce251d7a13d40c45a8b4d8
황성욱 <dnrdl1109@gmail.com>
2026-09-14 23:33:04 +0900

    refactor(web): split attendance check-in responsibilities

---
+++ apps/vada-web/src/screens/attendance-check-in/config.ts
@@ -0,0 +1,8 @@
+export const ATTENDANCE_CHECK_IN_NODE = {
+  brand: '30:7355',
+  head: '30:7361',
+  guide: '30:7369',
+  name: '30:7373',
+  studentNumber: '30:7378',
+  submit: '30:7383',
+} as const
